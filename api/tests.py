3266f270215291e322a1ba69a715f06fcdad4b0b
jamestha3d <jamesikediashi@gmail.com>
2022-11-06 05:03:55 -0500

    more tests

--- api/tests.py
+++ api/tests.py
@@ -19,8 +19,13 @@ class ListDepartmentsTest(APITestCase):
             email="juniordoe@user.com",
             password="Password@123"
             )
-            
-        
+        self.employee = {
+            "first_name": "Mr",
+            "last_name": "test",
+            "position": "tester",
+            "salary": 0,
+            "age": 0,
+        }
 
     def test_list_departments(self):
         view = listDepartments
@@ -33,8 +38,19 @@ class ListDepartmentsTest(APITestCase):
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_list_employees_with_user(self):
+    def test_list_employees_authenticated(self):
         request = self.factory.get(self.url)
         force_authenticate(request, user=self.user)
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_add_employee_authenticated(self):
+        request = self.factory.post(reverse('add'), self.employee)
+        force_authenticate(request, user=self.user)
+        response = addEmployee(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_add_employee_unauthenticated(self):
+        request = self.factory.post(reverse('add'), self.employee)
+        response = addEmployee(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
